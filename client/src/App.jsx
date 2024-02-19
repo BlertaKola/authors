5287262e8870d598b5c2528bb435ed4350d1e91d
BlertaKola <blertakola4@gmail.com>
2024-02-19 23:38:03 +0100

    first push

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Main from './views/Main';
+import Author from './components/Author';
+
+function App() {
+  const [authors, setAuthors] = useState([])
+
+  return (
+    <>
+      
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/authors" element={<Main authors={authors} setAuthors={setAuthors}></Main>} />
+        <Route path='/authors/:authorId/edit' element={<Author/>}/>
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
